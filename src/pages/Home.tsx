70788e160a86799a676e8cb669ef6f25ff27b1a6
abdullohqurbon0v <abdullohqurbonov332@gmail.com>
2025-03-26 18:33:55 +0500

    goof

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { motion } from "framer-motion";
+import { motion, useAnimation } from "framer-motion";
 import {
   RiBarChartLine,
   RiComputerLine,
@@ -12,6 +12,7 @@ import {
   AccordionTrigger,
 } from "../components/ui/accordion";
 
+// Тип для карточек
 interface CardType {
   subtitle: string;
   description: string;
@@ -51,6 +52,7 @@ const cards: CardType[] = [
   },
 ];
 
+// Варианты анимации для секций
 const sectionVariants = {
   hidden: { opacity: 0, y: 30 },
   visible: {
@@ -65,6 +67,7 @@ const sectionVariants = {
   },
 };
 
+// Варианты анимации для контейнеров
 const containerVariants = {
   hidden: { opacity: 0, y: 20 },
   visible: {
@@ -77,19 +80,43 @@ const containerVariants = {
     },
   },
 };
-
-const childVariants = {
-  hidden: { opacity: 0, y: 15 },
+const textVariants = {
+  hidden: { opacity: 0 },
+  visible: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.05,
+    },
+  },
+};
+const letterVariants = {
+  hidden: { opacity: 0, y: 20 },
   visible: {
     opacity: 1,
     y: 0,
     transition: {
-      duration: 0.5,
+      duration: 0.1,
       ease: "easeOut",
     },
   },
 };
 
+// Варианты анимации для параграфов
+const paragraphVariants = {
+  hidden: { opacity: 0, y: 15 },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.8,
+      ease: "easeOut",
+      type: "spring",
+      stiffness: 80,
+    },
+  },
+};
+
+// Варианты анимации для кнопок
 const buttonVariants = {
   hidden: { scale: 0.9, opacity: 0 },
   visible: {
@@ -107,9 +134,33 @@ const cardVariants = {
     opacity: 1,
     scale: 1,
     y: 0,
-    transition: { duration: 0.6, ease: "easeOut" },
+    transition: { duration: 0 / 3, ease: "easeOut" },
   },
   hover: { scale: 1.02, transition: { duration: 0.3 } },
+};
+
+const AnimatedText = ({
+  text,
+  className,
+}: {
+  text: string;
+  className?: string;
+}) => {
+  return (
+    <motion.div
+      className={className}
+      variants={textVariants}
+      initial="hidden"
+      whileInView="visible"
+      viewport={{ once: true }}
+    >
+      {text.split("").map((char, index) => (
+        <motion.span key={index} variants={letterVariants}>
+          {char}
+        </motion.span>
+      ))}
+    </motion.div>
+  );
 };
 
 const Home = () => {
@@ -139,22 +190,24 @@ const Home = () => {
         "In a fast-paced industry, standing still is not an option. We test new strategies, adopt cutting-edge tools, and constantly refine our approach to stay ahead of the competition.",
     },
   ];
+
   return (
     <main className="space-y-20 sm:space-y-40 w-full">
+      {/* Секция Home */}
       <motion.section
         id="home"
-        className="min-h-[80vh] relative flex flex-col justify-center items-center space-y-5 sm:space-y-6 "
+        className="min-h-[80vh] relative flex flex-col justify-center items-center space-y-5 sm:space-y-6"
         variants={sectionVariants}
         initial="hidden"
         whileInView="visible"
-        viewport={{ once: false, amount: 0.2 }}
+        viewport={{ once: true, amount: 0.2 }}
       >
         <video
           autoPlay
           loop
           muted
           playsInline
-          className="absolute top-0 left-0 w-full h-[90vh] object-cover "
+          className="absolute top-0 left-0 w-full h-[90vh] object-cover"
         >
           <source src="/4408477.mp4" type="video/mp4" />
           Your browser does not support the video tag.
@@ -163,15 +216,13 @@ const Home = () => {
           className="flex flex-col space-y-6 items-center relative z-10 pt-44"
           variants={containerVariants}
         >
-          <motion.h1
+          <AnimatedText
+            text="Turn Clicks into Customers, and Customers into Brand Loyalist"
             className="text-2xl md:text-4xl lg:text-5xl font-extrabold bg-gradient-to-r from-purple-200 to-blue-300 bg-clip-text tracking-tight text-transparent max-w-[100%] sm:max-w-5xl text-center"
-            variants={childVariants}
-          >
-            Turn Clicks into Customers, and Customers into Brand Loyalist
-          </motion.h1>
+          />
           <motion.p
             className="text-base sm:text-lg md:text-3xl text-gray-200 text-center max-w-5xl mx-auto"
-            variants={childVariants}
+            variants={paragraphVariants}
           >
             We drive traffic, increase conversions, and scale your online store
             through data-driven strategies.
@@ -186,27 +237,27 @@ const Home = () => {
           </motion.button>
         </motion.div>
       </motion.section>
+
+      {/* Секция Services */}
       <motion.section
         id="services"
         className="min-h-screen p-4 sm:p-6 lg:p-10 max-w-[1650px] mx-auto flex flex-col lg:flex-row justify-between items-start gap-6 lg:gap-8"
         variants={sectionVariants}
         initial="hidden"
         whileInView="visible"
-        viewport={{ once: false, amount: 0.2 }}
+        viewport={{ once: true, amount: 0.2 }}
       >
         <motion.div
           className="space-y-4 w-full lg:w-1/2 block md:sticky top-20 lg:top-80"
           variants={containerVariants}
         >
-          <motion.h1
+          <AnimatedText
+            text="Your Getaway to Infinite Possibilities"
             className="text-3xl sm:text-4xl lg:text-6xl bg-gradient-to-r from-purple-200 to-blue-300 bg-clip-text tracking-tight text-transparent font-bold drop-shadow-lg mt-32"
-            variants={childVariants}
-          >
-            Your Getaway to Infinite Possibilities
-          </motion.h1>
+          />
           <motion.p
             className="text-gray-200 text-sm sm:text-base lg:text-lg mb-4 drop-shadow-md max-w-md"
-            variants={childVariants}
+            variants={paragraphVariants}
           >
             We help brands optimize their digital presence and maximize revenue
             through advanced marketing strategies.
@@ -232,7 +283,7 @@ const Home = () => {
               whileHover="hover"
               initial="hidden"
               whileInView="visible"
-              viewport={{ once: false, amount: 0.3 }}
+              viewport={{ once: true, amount: 0.3 }}
             >
               <div className="absolute inset-0 bg-gradient-to-br from-[rgba(74,36,157,0.1)] via-[rgba(23,37,84,0.1)] to-[rgba(147,51,234,0.1)] opacity-0 hover:opacity-100 transition-opacity duration-300 rounded-xl" />
               <img
@@ -241,16 +292,16 @@ const Home = () => {
                 className="w-20"
               />
               <motion.div
-                className="relative z-10 font-semibold rounded-md inline-block px-2 sm:px-3 py-1 text-xs sm:text-2xl hover:drop-shadow-md "
-                variants={childVariants}
+                className="relative z-10 font-semibold rounded-md inline-block px-2 sm:px-3 py-1 text-xs sm:text-2xl hover:drop-shadow-md"
+                variants={cardVariants}
               >
-                <p>{item.company_name}</p>
+                <AnimatedText text={item.company_name} />
               </motion.div>
               <motion.div
                 className="relative z-10 rounded-xl max-w-[90%] space-y-2 sm:space-y-3 p-3"
-                variants={childVariants}
+                variants={paragraphVariants}
               >
-                <p className="text-sm w-full text-white shadow-none  drop-shadow-none">
+                <p className="text-sm w-full text-white shadow-none drop-shadow-none">
                   {item.description}
                 </p>
               </motion.div>
@@ -258,26 +309,25 @@ const Home = () => {
           ))}
         </motion.div>
       </motion.section>
+
+      {/* Секция Process */}
       <motion.section
         id="process"
         className="min-h-screen p-6 sm:p-10 flex items-center justify-center"
         variants={sectionVariants}
         initial="hidden"
         whileInView="visible"
-        viewport={{ once: false, amount: 0.3 }}
+        viewport={{ once: true, amount: 0.3 }}
       >
         <motion.div
           className="max-w-[1650px] mx-auto flex flex-col space-y-5 items-center"
           variants={containerVariants}
         >
-          <motion.p
+          <AnimatedText
+            text="VestraX is the proven e-commerce growth partner for Sports apparel & equipment brands ready to power past 8 figures"
             className="text-center text-4xl sm:text-5xl font-bold bg-gradient-to-r from-purple-200 to-blue-300 bg-clip-text tracking-tight text-transparent"
-            variants={childVariants}
-          >
-            VestraX is the proven e-commerce growth partner for Sports apparel &
-            equipment brands ready to power past 8 figures
-          </motion.p>
-          <motion.div variants={childVariants}>
+          />
+          <motion.div variants={cardVariants}>
             <motion.button
               className="px-6 py-4 bg-black text-white rounded-lg shadow-lg shadow-blue-500/30 hover:shadow-blue-500/50 transition-shadow duration-300 cursor-pointer"
               variants={buttonVariants}
@@ -289,13 +339,15 @@ const Home = () => {
           </motion.div>
         </motion.div>
       </motion.section>
+
+      {/* Секция About */}
       <motion.section
         id="about"
         className="min-h-screen p-6 sm:p-10 z-50 opacity-100"
         variants={sectionVariants}
         initial="hidden"
         whileInView="visible"
-        viewport={{ once: false, amount: 0.3 }}
+        viewport={{ once: true, amount: 0.3 }}
       >
         <motion.div
           className="max-w-[1650px] mx-auto flex flex-col md:flex-row gap-8 mt-32"
@@ -303,7 +355,7 @@ const Home = () => {
         >
           <motion.div
             className="w-full md:w-1/2 bg-gray-200 h-64 md:h-auto"
-            variants={childVariants}
+            variants={cardVariants}
           >
             <video
               src="about.mp4"
@@ -315,41 +367,37 @@ const Home = () => {
             />
           </motion.div>
           <motion.div className="w-full md:w-1/3" variants={containerVariants}>
-            <motion.h4
+            <AnimatedText
+              text="About VestraX"
               className="text-xl font-semibold mb-2"
-              variants={childVariants}
-            >
-              About VestraX
-            </motion.h4>
-            <motion.p className="font-medium mb-4 " variants={childVariants}>
-              Strategize, Optimize, Dominate
-            </motion.p>
-            <motion.p className="mb-4" variants={childVariants}>
+            />
+            <AnimatedText
+              text="Strategize, Optimize, Dominate"
+              className="font-medium mb-4"
+            />
+            <motion.p className="mb-4" variants={paragraphVariants}>
               The sports apparel and equipment industry is crowded, making it
               tough for brands to stand out and reach the next revenue
               milestone. Many businesses struggle with inconsistent sales,
               rising ad costs, and fierce competition.
             </motion.p>
-            <motion.p className="mb-4" variants={childVariants}>
+            <motion.p className="mb-4" variants={paragraphVariants}>
               Without a specialized marketing strategy, even the most innovative
               products can get lost in the noise. Brands end up wasting money on
               poorly targeted ads, while missing out on loyal, lifelong fans.
             </motion.p>
-            <motion.p className="mb-4" variants={childVariants}>
+            <motion.p className="mb-4" variants={paragraphVariants}>
               At VestraX, we combine data-driven insights with our passion for
               sports to help you scale efficiently. Our targeted ad campaigns,
               conversion optimization, and influencer partnerships ensure your
               brand thrives in a hyper-competitive market—helping you move past
               6- or 7-figure ceilings and onto 8-figure success.
             </motion.p>
-
-            <motion.p
+            <AnimatedText
+              text="Why Sports?"
               className="font-semibold mt-6 mb-2"
-              variants={childVariants}
-            >
-              Why Sports?
-            </motion.p>
-            <motion.p className="mb-4" variants={childVariants}>
+            />
+            <motion.p className="mb-4" variants={paragraphVariants}>
               Our team is made up of former athletes, fitness enthusiasts, and
               marketing pros who understand the mindset of sports consumers.
               We’ve seen firsthand how community, authenticity, and performance
@@ -368,58 +416,69 @@ const Home = () => {
           </motion.div>
         </motion.div>
       </motion.section>
+
+      {/* Секция Philosophy */}
       <motion.section
-        id="philasophy"
-        className="p-6 sm:p-10 mt-96 "
+        id="philosophy"
+        className="p-6 sm:p-10 mt-96"
         variants={sectionVariants}
         initial="hidden"
         whileInView="visible"
-        viewport={{ once: false, amount: 0.3 }}
+        viewport={{ once: true, amount: 0.3 }}
       >
-        <motion.h1
+        <AnimatedText
+          text="Our Work Philosophy"
           className="text-2xl sm:text-3xl font-bold text-center mt-56"
-          variants={childVariants}
-        >
-          Our Work Philosophy
-        </motion.h1>
+        />
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 mt-8">
           {work.map((item, index) => {
             const Icon = item.icon;
             return (
               <motion.div
                 key={index}
-                className={`flex flex-col items-center text-center p-4`}
-                variants={childVariants}
+                className="flex flex-col items-center text-center p-4"
+                variants={cardVariants}
               >
-                <Icon className="text-4xl  mb-4" />
-                <h2 className="text-lg font-bold  mb-2">{item.title}</h2>
-                <p className="text-sm ">{item.description}</p>
+                <Icon className="text-4xl mb-4" />
+                <AnimatedText
+                  text={item.title}
+                  className="text-lg font-bold mb-2"
+                />
+                <motion.p className="text-sm" variants={paragraphVariants}>
+                  {item.description}
+                </motion.p>
               </motion.div>
             );
           })}
         </div>
       </motion.section>
+
+      {/* Секция FAQ */}
       <motion.section
         id="faq"
-        className="min-h-screen max-w-[800px] flex flex-col justify-center mx-auto space-y-6  text-white p-6"
+        className="min-h-screen max-w-[800px] flex flex-col justify-center mx-auto space-y-6 text-white p-6"
         variants={sectionVariants}
         initial="hidden"
         whileInView="visible"
-        viewport={{ once: false, amount: 0.3 }}
+        viewport={{ once: true, amount: 0.3 }}
       >
-        <h1 className="text-center text-4xl font-bold text-white">
-          Frequently Asked Questions
-        </h1>
-        <p className="text-center text-lg text-gray-400 font-medium">
+        <AnimatedText
+          text="Frequently Asked Questions"
+          className="text-center text-4xl font-bold text-white"
+        />
+        <motion.p
+          className="text-center text-lg text-gray-400 font-medium"
+          variants={paragraphVariants}
+        >
           Everything you need to know about our services
-        </p>
+        </motion.p>
         <Accordion type="single" collapsible className="w-full space-y-4">
           <AccordionItem
             value="item-1"
             className="border border-gray-500 px-5 py-3 rounded-lg bg-transparent"
           >
             <AccordionTrigger className="cursor-pointer text-lg font-semibold text-white transition-colors flex justify-between items-center w-full">
-              How does AI enhance marketing effectiveness?
+              <AnimatedText text="How does AI enhance marketing effectiveness?" />
               <span className="accordion-arrow" />
             </AccordionTrigger>
             <AccordionContent className="text-gray-300 mt-2">
@@ -434,7 +493,7 @@ const Home = () => {
             className="border border-gray-500 px-5 py-3 rounded-lg bg-transparent"
           >
             <AccordionTrigger className="cursor-pointer text-lg font-semibold text-white transition-colors flex justify-between items-center w-full">
-              What sets your agency apart from the competition?
+              <AnimatedText text="What sets your agency apart from the competition?" />
               <span className="accordion-arrow" />
             </AccordionTrigger>
             <AccordionContent className="text-gray-300 mt-2">
@@ -449,7 +508,7 @@ const Home = () => {
             className="border border-gray-500 px-5 py-3 rounded-lg bg-transparent"
           >
             <AccordionTrigger className="cursor-pointer text-lg font-semibold text-white transition-colors flex justify-between items-center w-full">
-              When can we expect to see tangible results?
+              <AnimatedText text="When can we expect to see tangible results?" />
               <span className="accordion-arrow" />
             </AccordionTrigger>
             <AccordionContent className="text-gray-300 mt-2">
@@ -464,7 +523,7 @@ const Home = () => {
             className="border border-gray-500 px-5 py-3 rounded-lg bg-transparent"
           >
             <AccordionTrigger className="cursor-pointer text-lg font-semibold text-white transition-colors flex justify-between items-center w-full">
-              Do you offer guaranteed results?
+              <AnimatedText text="Do you offer guaranteed results?" />
               <span className="accordion-arrow" />
             </AccordionTrigger>
             <AccordionContent className="text-gray-300 mt-2">
@@ -479,7 +538,7 @@ const Home = () => {
             className="border border-gray-500 px-5 py-3 rounded-lg bg-transparent"
           >
             <AccordionTrigger className="cursor-pointer text-lg font-semibold text-white transition-colors flex justify-between items-center w-full">
-              What’s your pricing structure?
+              <AnimatedText text="What’s your pricing structure?" />
               <span className="accordion-arrow" />
             </AccordionTrigger>
             <AccordionContent className="text-gray-300 mt-2">
